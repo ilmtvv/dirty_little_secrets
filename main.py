b934c155b6cc283d2e45c94c3b72fd9d5c5fc0fc
ilmtvv <napalkah@gmail.com>
2024-04-17 21:58:25 +0300

    c

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ app = FastAPI()     # app for manager of secrets
 
 @app.post('/generate', response_model=SecretOutput)
 def generate_secret(secret_input: SecretInput):
+    """
+    url for generate secret
+    :param secret_input:
+    :return: secret_key_json
+    """
     hashed_passphrase = get_hashed_passphrase(secret_input.passphrase)
     secret_key = save_secret(secret_input.secret, hashed_passphrase, secret_input.ttl_day)
     return {'secret_key': secret_key}
@@ -16,6 +21,11 @@ def generate_secret(secret_input: SecretInput):
 
 @app.post('/secrets/{secret_key}', response_model=SecretPayload)
 async def get_secret(secret_key: str):
+    """
+    url for get secret
+    :param secret_key:
+    :return:
+    """
     secret = get_and_delete_secret(secret_key)
     if secret is None:
         raise HTTPException(status_code=404, detail='Secret not found or passphrase incorrect')
