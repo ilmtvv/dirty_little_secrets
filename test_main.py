a35b7c7d1bbb54806c1bb82089f62f3563812daa
ilmtvv <napalkah@gmail.com>
2024-04-17 22:31:37 +0300

    c

--- test_main.py
+++ test_main.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_secret():
     data = {
         'secret': 'test',
-        'passphrase': 'test',
+        'pass_phrase': 'test',
         'tll': 2
     }
 
@@ -16,10 +16,13 @@ def test_secret():
     response_create_secret = client.post('/generate/', json=data)
     assert response_create_secret.status_code == 200
 
+    data = {
+        'pass_phrase': 'test',
+    }
     # test for get secret #
-    responce_get_secret = client.post(f'/secrets/{response_create_secret.json()["secret_key"]}')
-    assert responce_get_secret.status_code == 200
+    response_get_secret = client.post(f'/secrets/{response_create_secret.json()["secret_key"]}', json=data)
+    assert response_get_secret.status_code == 200
 
     # test for get after get secret #
-    responce_get_secret = client.post(f'/secrets/{response_create_secret.json()["secret_key"]}')
-    assert responce_get_secret.status_code == 404
+    response_get_secret = client.post(f'/secrets/{response_create_secret.json()["secret_key"]}', json=data)
+    assert response_get_secret.status_code == 404
